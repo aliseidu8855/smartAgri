0708e9e93afa901437682fe9e89b85c6df0b2ad1
aliseidu8855 <aliseidu8855@gmail.com>
2025-07-18 01:16:44 +0000

    Update .gitignore to exclude database files and modify ALLOWED_HOSTS in settings.py for development

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+927jp5ik3_$_cbqao%si3wx*4ml72wx7j7=xa^z1ve7$a9o^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
